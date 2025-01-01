a0895317390631fbd02a4d09d4f39351057a04b5
SphereKuribob <kurribobsphere@gmail.com>
2025-01-01 00:49:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.gallery img').forEach((img) => {
         img.addEventListener('click', () => {
             img.classList.toggle('grayscale-off'); // Toggle the grayscale-off class
+
+            // Add or remove a border based on the grayscale-off class
+            if (img.classList.contains('grayscale-off')) {
+                img.style.border = '3px solid #FFD700'; // Add border
+            } else {
+                img.style.border = 'none'; // Remove border
+            }
         });
     });
 });
